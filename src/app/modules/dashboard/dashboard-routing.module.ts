75645a4578e7342b43b0ed3b5d779903c28c9ca2
Mohammad <mohammedaktaa1995@gmail.com>
2021-10-07 13:50:41 +0300

    changes

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       {
         path: '',
--- src/app/modules/entities/entities-routing.module.ts
+++ src/app/modules/entities/entities-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {
     path: ':entity',
     component: EntitiesComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       {
         path: '',
